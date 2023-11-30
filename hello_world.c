81a99e496062d37a1a8e60262e3c8491cf1e418c
MoritaGenki <morita@na-s.jp>
2023-11-30 11:51:40 +0900

    first commit

---
+++ hello_world.c
@@ -0,0 +1,14 @@
+//cmake -DPICO_SDK_PATH=../pico-sdk/ -DPICO_TOOLCHAIN_PATH=/opt/homebrew ..
+//sudo screen /dev/tty.usbmodem11101
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+    stdio_init_all();
+    for(int i=0;i<60;i++){
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+    printf("END");
+    return 0;
+}
