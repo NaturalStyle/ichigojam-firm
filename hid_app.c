b29b42452768f1da91b348a49e71eb3db0742681
MoritaGenki <morita@na-s.jp>
2023-12-25 15:52:22 +0900

    hid.hの必要な部分(USBのデータ→アスキー変換)だけ抜粋して上書き

--- hid_app.c
+++ hid_app.c
@@ -25,6 +25,7 @@
 
 #include "bsp/board.h"
 #include "tusb.h"
+#include "hid.h"
 
  //--------------------------------------------------------------------+
  // MACRO TYPEDEF CONSTANT ENUM DECLARATION
