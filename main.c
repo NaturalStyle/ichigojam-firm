d9594fb4e8fb3e4a9997f9c3ef27e991510c2512
MoritaGenki <morita@na-s.jp>
2024-01-10 10:02:54 +0900

    初期化の処理を関数化

--- main.c
+++ main.c
@@ -62,50 +62,104 @@ extern uint8* vram;
 struct keyflg_def key_flg;
 
 void core1_main() {
-	dvi_register_irqs_this_core(&dvi0, DMA_IRQ_0);
-	dvi_start(&dvi0);
-	dvi_scanbuf_main_16bpp(&dvi0);
-	__builtin_unreachable();
+    dvi_register_irqs_this_core(&dvi0, DMA_IRQ_0);
+    dvi_start(&dvi0);
+    dvi_scanbuf_main_16bpp(&dvi0);
+    __builtin_unreachable();
 }
 
 void core1_scanline_callback() {
-	// Discard any scanline pointers passed back
-	uint16_t* bufptr;
-	while (queue_try_remove_u32(&dvi0.q_colour_free, &bufptr))
-		;
-	// // Note first two scanlines are pushed before DVI start
-	static uint scanline = 2;
-	bufptr = &framebuf[FRAME_WIDTH * scanline];
-	queue_add_blocking_u32(&dvi0.q_colour_valid, &bufptr);
-	scanline = (scanline + 1) % FRAME_HEIGHT;
+    // Discard any scanline pointers passed back
+    uint16_t* bufptr;
+    while (queue_try_remove_u32(&dvi0.q_colour_free, &bufptr))
+        ;
+    // // Note first two scanlines are pushed before DVI start
+    static uint scanline = 2;
+    bufptr = &framebuf[FRAME_WIDTH * scanline];
+    queue_add_blocking_u32(&dvi0.q_colour_valid, &bufptr);
+    scanline = (scanline + 1) % FRAME_HEIGHT;
 }
 
 //指定したvramの位置の1文字をframebufに反映する
 void vram_to_framebuf(int vram_x, int vram_y, int inversion) {
-	int c = vram[vram_y * CHAR_COLS + vram_x];
-	for (int y = 0; y < FONT_SIZE; y++) {
-		int line = CHAR_PATTERN[c * FONT_SIZE + y] ^ inversion;
-		for (int x = 0; x < FONT_SIZE; x++) {
-			int pixel = (line & (0x80 >> x)) ? 0xffff : 0x0000;
-			framebuf[(y + vram_y * FONT_SIZE + MARGIN_HEIGHT) * FRAME_WIDTH
-				+ (x + vram_x * FONT_SIZE + MARGIN_WIDTH)] = pixel;
-		}
-	}
+    int c = vram[vram_y * CHAR_COLS + vram_x];
+    for (int y = 0; y < FONT_SIZE; y++) {
+        int line = CHAR_PATTERN[c * FONT_SIZE + y] ^ inversion;
+        for (int x = 0; x < FONT_SIZE; x++) {
+            int pixel = (line & (0x80 >> x)) ? 0xffff : 0x0000;
+            framebuf[(y + vram_y * FONT_SIZE + MARGIN_HEIGHT) * FRAME_WIDTH
+                + (x + vram_x * FONT_SIZE + MARGIN_WIDTH)] = pixel;
+        }
+    }
 }
 
 void vram_to_framebuf_all(bool visible_cursor) {
-	int inversion;
-	for (int y = 0;y < _g.screenh;y++) {
-		for (int x = 0;x < _g.screenw;x++) {
-			inversion = 0x00;
-			if (visible_cursor && x == _g.cursorx && y == _g.cursory) {
-				inversion = 0xf0;
-			}
-			vram_to_framebuf(x, y, inversion);
-		}
-	}
+    int inversion;
+    for (int y = 0;y < _g.screenh;y++) {
+        for (int x = 0;x < _g.screenw;x++) {
+            inversion = 0x00;
+            if (visible_cursor && x == _g.cursorx && y == _g.cursory) {
+                inversion = 0xf0;
+            }
+            vram_to_framebuf(x, y, inversion);
+        }
+    }
 }
 
+void pico_init() {
+    board_init();
+    stdio_init_all();
+
+    vreg_set_voltage(VREG_VSEL);
+    sleep_ms(10);
+#ifdef RUN_FROM_CRYSTAL
+    set_sys_clock_khz(12000, true);
+#else
+    // Run system at TMDS bit clock
+    set_sys_clock_khz(DVI_TIMING.bit_clk_khz, true);
+#endif
+
+    setup_default_uart();
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 1);
+
+    dvi0.timing = &DVI_TIMING;
+    dvi0.ser_cfg = DVI_DEFAULT_SERIAL_CONFIG;
+    dvi0.scanline_callback = core1_scanline_callback;
+    dvi_init(&dvi0, next_striped_spin_lock_num(), next_striped_spin_lock_num());
+
+    // Once we've given core 1 the framebuffer, it will just keep on displaying
+    // it without any intervention from core 0
+    sprite_fill16(framebuf, 0x0000, FRAME_WIDTH * FRAME_HEIGHT);
+    uint16_t* bufptr = framebuf;
+    queue_add_blocking_u32(&dvi0.q_colour_valid, &bufptr);
+    bufptr += FRAME_WIDTH;
+    queue_add_blocking_u32(&dvi0.q_colour_valid, &bufptr);
+
+    multicore_launch_core1(core1_main);
+
+    // init host stack on configured roothub port
+    tuh_init(BOARD_TUH_RHPORT);
+}
+
+void ichigojam_init() {
+    _g.screenw = CHAR_COLS;
+    _g.screenh = CHAR_ROWS;
+    screen_clear();
+
+    for (int y = 0; y < CHAR_ROWS; ++y) {
+        for (int x = 0; x < CHAR_COLS; ++x) {
+            // vram[y * CHAR_COLS + x] = (y * CHAR_COLS + x) % (256 - 32) + 32;
+            vram[y * CHAR_COLS + x] = 0;
+        }
+    }
+    vram_to_framebuf_all(true);
+    key_clearKey();
+    key_flg.caps = true;
+    key_flg.insert = 0;
+}
 
 
 
@@ -148,57 +202,8 @@ STATIC void exec(char* s) {
 
 
 int main() {
-	board_init();
-	stdio_init_all();
-
-	vreg_set_voltage(VREG_VSEL);
-	sleep_ms(10);
-#ifdef RUN_FROM_CRYSTAL
-	set_sys_clock_khz(12000, true);
-#else
-	// Run system at TMDS bit clock
-	set_sys_clock_khz(DVI_TIMING.bit_clk_khz, true);
-#endif
-
-	setup_default_uart();
-
-	gpio_init(LED_PIN);
-	gpio_set_dir(LED_PIN, GPIO_OUT);
-	gpio_put(LED_PIN, 1);
-
-	printf("Configuring DVI\n");
-
-	dvi0.timing = &DVI_TIMING;
-	dvi0.ser_cfg = DVI_DEFAULT_SERIAL_CONFIG;
-	dvi0.scanline_callback = core1_scanline_callback;
-	dvi_init(&dvi0, next_striped_spin_lock_num(), next_striped_spin_lock_num());
-
-	// Once we've given core 1 the framebuffer, it will just keep on displaying
-	// it without any intervention from core 0
-	sprite_fill16(framebuf, 0x0000, FRAME_WIDTH * FRAME_HEIGHT);
-	uint16_t* bufptr = framebuf;
-	queue_add_blocking_u32(&dvi0.q_colour_valid, &bufptr);
-	bufptr += FRAME_WIDTH;
-	queue_add_blocking_u32(&dvi0.q_colour_valid, &bufptr);
-
-	printf("Core 1 start\n");
-	multicore_launch_core1(core1_main);
-
-	// init host stack on configured roothub port
-	tuh_init(BOARD_TUH_RHPORT);
-	_g.screenw = CHAR_COLS;
-	_g.screenh = CHAR_ROWS;
-	screen_clear();
-
-	for (int y = 0; y < CHAR_ROWS; ++y) {
-		for (int x = 0; x < CHAR_COLS; ++x) {
-			vram[y * CHAR_COLS + x] = (y * CHAR_COLS + x) % (256 - 32) + 32;
-		}
-	}
-	vram_to_framebuf_all(true);
-	key_clearKey();
-    key_flg.caps = true;
-    key_flg.insert = 0;
+    pico_init();
+    ichigojam_init();
 
 	char* linebuf = (char*)ram + OFFSET_RAM_LINEBUF;
 	if (*linebuf) {
