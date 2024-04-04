2a38e8f27aa9232edae5d9a0dab9e0206313e2e6
MoritaGenki <morita@na-s.jp>
2024-04-04 10:50:29 +0900

    printfの無効化の仕方を変更

--- hid_app.c
+++ hid_app.c
@@ -25,6 +25,13 @@
 
  //pico-examples/usb/host/host_cdc_msc_hid/hid_app.cを拡張
 
+#define HID_APP_C_NO_PRINTF
+#ifdef HID_APP_C_NO_PRINTF
+#define printf //printf
+#define TU_LOG1 //TU_LOG1
+
+#endif
+
 #ifndef __HID_APP_C__
 #define __HID_APP_C__
 
@@ -79,20 +86,20 @@ void hid_app_task(void)
 // therefore report_desc = NULL, desc_len = 0
 void tuh_hid_mount_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* desc_report, uint16_t desc_len)
 {
-    // printf("HID device address = %d, instance = %d is mounted\r\n", dev_addr, instance);
+    printf("HID device address = %d, instance = %d is mounted\r\n", dev_addr, instance);
 
     //   Interface protocol (hid_interface_protocol_enum_t)
     const char* protocol_str[] = { "None", "Keyboard", "Mouse" };
     uint8_t const itf_protocol = tuh_hid_interface_protocol(dev_addr, instance);
 
-    // printf("HID Interface Protocol = %s\r\n", protocol_str[itf_protocol]);
+    printf("HID Interface Protocol = %s\r\n", protocol_str[itf_protocol]);
 
     // By default host stack will use activate boot protocol on supported interface.
     // Therefore for this simple example, we only need to parse generic report descriptor (with built-in parser)
     if (itf_protocol == HID_ITF_PROTOCOL_NONE)
     {
         hid_info[instance].report_count = tuh_hid_parse_report_descriptor(hid_info[instance].report_info, MAX_REPORT, desc_report, desc_len);
-        // printf("HID has %u reports \r\n", hid_info[instance].report_count);
+        printf("HID has %u reports \r\n", hid_info[instance].report_count);
     }
 
     uint8_t leds = 0;
@@ -103,14 +110,14 @@ void tuh_hid_mount_cb(uint8_t dev_addr, uint8_t instance, uint8_t const* desc_re
     // tuh_hid_report_received_cb() will be invoked when report is available
     if (!tuh_hid_receive_report(dev_addr, instance))
     {
-        // printf("Error: cannot request to receive report\r\n");
+        printf("Error: cannot request to receive report\r\n");
     }
 }
 
 // Invoked when device with hid interface is un-mounted
 void tuh_hid_umount_cb(uint8_t dev_addr, uint8_t instance)
 {
-    // printf("HID device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
+    printf("HID device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
 }
 
 // Invoked when received report from device via interrupt endpoint
@@ -139,7 +146,7 @@ void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance, uint8_t cons
     // continue to request to receive report
     if (!tuh_hid_receive_report(dev_addr, instance))
     {
-        // printf("Error: cannot request to receive report\r\n");
+        printf("Error: cannot request to receive report\r\n");
     }
 }
 
@@ -251,33 +258,33 @@ void cursor_movement(int8_t x, int8_t y, int8_t wheel)
     // Move X using ansi escape
     if (x < 0)
     {
-        // printf(ANSI_CURSOR_BACKWARD(% d), (-x)); // move left
+        printf(ANSI_CURSOR_BACKWARD(% d), (-x)); // move left
     } else if (x > 0)
     {
-        // printf(ANSI_CURSOR_FORWARD(% d), x); // move right
+        printf(ANSI_CURSOR_FORWARD(% d), x); // move right
     }
 
     // Move Y using ansi escape
     if (y < 0)
     {
-        // printf(ANSI_CURSOR_UP(% d), (-y)); // move up
+        printf(ANSI_CURSOR_UP(% d), (-y)); // move up
     } else if (y > 0)
     {
-        // printf(ANSI_CURSOR_DOWN(% d), y); // move down
+        printf(ANSI_CURSOR_DOWN(% d), y); // move down
     }
 
     // Scroll using ansi escape
     if (wheel < 0)
     {
-        // printf(ANSI_SCROLL_UP(% d), (-wheel)); // scroll up
+        printf(ANSI_SCROLL_UP(% d), (-wheel)); // scroll up
     } else if (wheel > 0)
     {
-        // printf(ANSI_SCROLL_DOWN(% d), wheel); // scroll down
+        printf(ANSI_SCROLL_DOWN(% d), wheel); // scroll down
     }
 
-    // printf("\r\n");
+    printf("\r\n");
 #else
-    // printf("(%d %d %d)\r\n", x, y, wheel);
+    printf("(%d %d %d)\r\n", x, y, wheel);
 #endif
 }
 
@@ -289,10 +296,12 @@ static void process_mouse_report(hid_mouse_report_t const* report)
     uint8_t button_changed_mask = report->buttons ^ prev_report.buttons;
     if (button_changed_mask & report->buttons)
     {
-        // printf(" %c%c%c ",
-        // report->buttons& MOUSE_BUTTON_LEFT ? 'L' : '-',
-        //     report->buttons& MOUSE_BUTTON_MIDDLE ? 'M' : '-',
-        //     report->buttons& MOUSE_BUTTON_RIGHT ? 'R' : '-');
+#ifndef HID_APP_C_NO_PRINTF
+        printf(" %c%c%c ",
+            report->buttons & MOUSE_BUTTON_LEFT ? 'L' : '-',
+            report->buttons & MOUSE_BUTTON_MIDDLE ? 'M' : '-',
+            report->buttons & MOUSE_BUTTON_RIGHT ? 'R' : '-');
+#endif
     }
 
     //------------- cursor movement -------------//
@@ -335,7 +344,7 @@ static void process_generic_report(uint8_t dev_addr, uint8_t instance, uint8_t c
 
     if (!rpt_info)
     {
-        // printf("Couldn't find the report info for this report !\r\n");
+        printf("Couldn't find the report info for this report !\r\n");
         return;
     }
 
@@ -410,4 +419,6 @@ void put_function_key(uint8_t key) {
     }
 }
 
+#undef printf
+#undef TU_LOG1
 #endif
